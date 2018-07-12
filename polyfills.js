fa0f4d106dd6f6096b5c02a975d4edda4cbff15c
Sumodh Nair <snair4@sapient.com>
2018-07-12 13:39:31 +0530

    more polyfills added~

--- polyfills.js
+++ polyfills.js
@@ -1,46 +1,53 @@
+const promiseAll = (array) =>  new Promise(function(resolve, reject){
+   let count = array.length,
+       result = [],
+       isDone = function() {
+                  if(--count == 0) {
+                    resolve(result);
+                  }
+                };
+
+   array.forEach(function(val,key){
+     val.then(function(data){
+       result.push(data);
+     },reject).then(isDone);
+   })
+ });
+
+
 Array.prototype.mymap = function(callback) {
 
-
-  var myarray = [];
+  let myarray = [];
   this.forEach(function(val, key) {
 
       myarray.push(callback(val));
   });
   return myarray;
-
 }
 
 Array.prototype.myfilter = function(callback) {
 
-
-  var filteredData = [];
-  console.log("callback", callback);
+  let filteredData = [];
   this.forEach(function(val, key) {
     if (callback(val))
       filteredData.push(val);
 
   });
   return filteredData;
-
 }
-
 
 Array.prototype.myReducer = function(callback, start) {
 
-  var result = typeof start != 'undefined' ? start : 0;
+  let result = typeof start != 'undefined' ? start : 0;
 
   this.forEach(function(val, key) {
-
     result = callback(result, val);
-
   });
   return result;
-
-
 }
 
 Array.prototype.unique = function() {
-  var uniqueArr = [];
+  let uniqueArr = [];
 
   this.forEach(function(val, key) {
     if (!uniqueArr.includes(val)) {
@@ -51,45 +58,11 @@ Array.prototype.unique = function() {
 
 }
 
-const promiseAll = function(array){
- return new Promise(function(resolve, reject){
-   var count = array.length;
-   var result = [];
-   var isDone = function() {
-     if(--count == 0) {
-       resolve(result);
-     }
-   }
-   array.forEach(function(val,key){
-     val.then(function(data){
-       result.push(data);
-     },reject).then(isDone);
-   })
- });
-}
-
-
 const reverseString = (input, index) => input.split('').reverse().join('');
 
-const returnRandomCombination = function(n) {
+const returnRandomCombination = (n) =>  n === 1 ? 0 : n === 2 ? 1 : 1 * returnRandomCombination(n - 1) + 2 * returnRandomCombination(n - 2) + 3;
 
-  if (n === 1) {
-
-    return 0;
-
-  } else if (n === 2) {
-
-    return 1;
-
-  } else {
-
-    return 1 * returnRandomCombination(n - 1) + 2 * returnRandomCombination(n - 2) + 3;
-
-  }
-
-}
-
-const fibonacciSeries = function(input) {
+const fibonacciSeries = (input) => {
 
   let fibonacciSeries = "";
   let arr = [];
